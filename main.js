b2480f7f93eba47af276c17bc633657bbb458f68
Kendall <kendallawills@gmail.com>
2023-07-28 14:17:12 -0400

    Repeat Tasks Manually

---
+++ main.js
@@ -0,0 +1,8 @@
+// Repeating Tasks Manually
+
+const vacationSpots = ['Hawaii', 'Australia', 'Ireland'];
+
+console.log(vacationSpots[0]);
+console.log(vacationSpots[1]);
+console.log(vacationSpots[2]);
+
