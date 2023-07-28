002e39d8c41bbaf046e5bc9d86cfd2b6f2b47689
Kendall <kendallawills@gmail.com>
2023-07-28 14:25:50 -0400

    For Loop

--- main.js
+++ main.js
@@ -6,3 +6,8 @@ console.log(vacationSpots[0]);
 console.log(vacationSpots[1]);
 console.log(vacationSpots[2]);
 
+// The For Loop
+
+for (let counter = 5; counter < 11; counter++) {
+    console.log(counter);
+}
